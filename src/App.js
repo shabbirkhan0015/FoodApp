deabfa9b0d0912e7f9dbd462830170b050088c5b
Shabbir Khan <shabbirkhan@Shabbirs-MacBook-Pro.local>
2023-12-24 23:25:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navigation from './components/Navigation';
+import SectionContainer from './components/SectionContainer';
+import {useState} from 'react'
 
 function App() {
+  const [filter, setfilter] =useState("all");
+  const handleClick = (e) => {
+    setfilter(e.target.name);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation handleClick={handleClick}/>
+      <SectionContainer filter={filter} />
     </div>
   );
 }
